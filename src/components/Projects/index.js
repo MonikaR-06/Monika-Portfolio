9c77f041ecba2218b785b6d8e51437b7babec214
monikaa06 <writetomonika06@gmail.com>
2024-07-08 20:27:10 +0530

    first commit

---
+++ src/components/Projects/index.js
@@ -0,0 +1,148 @@
+import React from "react";
+import { useState } from "react";
+import {
+  Container,
+  Wrapper,
+  Title,
+  Desc,
+  CardContainer,
+  ToggleButtonGroup,
+  ToggleButton,
+  Divider,
+} from "./ProjectsStyle";
+import ProjectCard from "../Cards/ProjectCards";
+import { projects } from "../../data/constants";
+
+const Projects = ({ openModal, setOpenModal }) => {
+  const [toggle, setToggle] = useState("all");
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+          I have practiced on Frontend development Projects. Here are some of my
+          projects.
+        </Desc>
+        <ToggleButtonGroup>
+          {toggle === "all" ? (
+            <ToggleButton active value="all" onClick={() => setToggle("all")}>
+              All
+            </ToggleButton>
+          ) : (
+            <ToggleButton value="all" onClick={() => setToggle("all")}>
+              All
+            </ToggleButton>
+          )}
+          <Divider />
+          {toggle === "Social Media App" ? (
+            <ToggleButton
+              active
+              value="Social Media App"
+              onClick={() => setToggle("Social Media App")}
+            >
+              Social Media App
+            </ToggleButton>
+          ) : (
+            <ToggleButton
+              value="Social Media App"
+              onClick={() => setToggle("Social Media App")}
+            >
+              Social Media App
+            </ToggleButton>
+          )}
+          <Divider />
+          {toggle === "Shopping Cart" ? (
+            <ToggleButton
+              active
+              value="Shopping Cart"
+              onClick={() => setToggle("Shopping Cart")}
+            >
+              Shopping Cart
+            </ToggleButton>
+          ) : (
+            <ToggleButton
+              value="Shopping Cart"
+              onClick={() => setToggle("Shopping Cart")}
+            >
+              Shopping Cart
+            </ToggleButton>
+          )}
+          <Divider />
+          {toggle === "Book Shopping" ? (
+            <ToggleButton
+              active
+              value="Book Shopping"
+              onClick={() => setToggle("Book Shopping")}
+            >
+              Book Shopping
+            </ToggleButton>
+          ) : (
+            <ToggleButton
+              value="Book Shopping"
+              onClick={() => setToggle("Book Shopping")}
+            >
+              Book Shopping
+            </ToggleButton>
+          )}
+          <Divider />
+          {toggle === "ToDo Application" ? (
+            <ToggleButton
+              active
+              value="ToDo Application"
+              onClick={() => setToggle("ToDo Application")}
+            >
+              ToDo Application
+            </ToggleButton>
+          ) : (
+            <ToggleButton
+              value="ToDo Application"
+              onClick={() => setToggle("ToDo Application")}
+            >
+              ToDo Application
+            </ToggleButton>
+            
+          )}
+          <Divider />
+          {toggle === "MERN Stack" ? (
+            <ToggleButton
+              active
+              value="MERN Stack"
+              onClick={() => setToggle("MERN Stack")}
+            >
+              MERN Stack
+            </ToggleButton>
+          ) : (
+            <ToggleButton
+              value="MERN Stack"
+              onClick={() => setToggle("MERN Stack")}
+            >
+              MERN Stack
+            </ToggleButton>
+            
+          )}
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === "all" &&
+            projects.map((project) => (
+              <ProjectCard
+                project={project}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
+            ))}
+          {projects
+            .filter((item) => item.category === toggle)
+            .map((project) => (
+              <ProjectCard
+                project={project}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Projects;
---
+++ src/utils/Themes.js
@@ -0,0 +1,24 @@
+export const darkTheme = {
+    bg:"#EBDB49",
+    bgLight: "#1C1E27",
+    primary:"#854CE6",
+    text_primary:"#F8F7F3",
+    text_secondary:"#F8F7F3",
+    text_color: "#070706",
+    text_white: "#FFFFFF",
+    card:"#171721",
+    card_light: '#191924',
+    button:"#F8F7F3",
+    white:"#FFFFFF",
+    black:"#000000",
+}
+
+export const lightTheme = {
+    bg:"#FFFFFF",
+    bgLight: "#f0f0f0",
+    primary:"#be1adb",
+    text_primary:"#111111",
+    text_secondary:"#48494a",
+    card:"#FFFFFF",
+    button:"#F8F7F3",
+}
